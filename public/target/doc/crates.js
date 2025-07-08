361f7a99ae120e46cd94eba3621b9530ccaee923
Carlitos <juanpana10ospina@gmail.com~>
2025-07-08 11:43:36 -0500

    push

---
+++ public/target/doc/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["public"];
+//{"start":21,"fragment_lengths":[8]}
---
+++ public/target/doc/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["public",["",[],["main.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[30]}
